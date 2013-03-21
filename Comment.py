da82c32bf71d371fd6b4a9bfb64cb7f66a499468
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2013-03-21 14:47:34 +0200

    Remove obsolete fields

--- Comment.py
+++ Comment.py
@@ -4,17 +4,21 @@ class Comment:
         self.stars = ""
         self.header = ""
         self.comment = ""
+
     def printAll(self):
-        print "\nNumber: %s\nHelpful: %s\nStars: %s\nHeader: %s\nComment:\n" %\
-            (self.number, self.helpful, self.stars, self.header),
+        print "\nHelpful: %s\nStars: %s\nHeader: %s\nComment:\n" %\
+            (self.helpful, self.stars, self.header),
         for line in self.comment:
             print "%s" % line
+
     def getComment(self):
         return "".join(self.comment)
+
     def __repr__(self):
-        return "\nNumber: %s\nHelpful: %s\nStars: %s\nHeader: %s\nComment: %s\n" %\
-            (self.number, self.helpful, self.stars, self.header, \
+        return "%s\nHelpful: %s\nStars: %s\nHeader: %s\nComment: %s\n" %\
+            (self.helpful, self.stars, self.header, \
             self.getComment())
+
     def getonelinecomment(self):
         return self.comment.replace("\n", " ")
 
