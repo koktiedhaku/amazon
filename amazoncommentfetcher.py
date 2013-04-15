31193f86fe9590d580ce6dc91d167a2a980cf8e7
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2013-04-15 23:30:08 +0300

    - Change output format a bit - There should be around 4170 comments per product 	o Therefore tablet_kindle_fire.txt can go - Complete cellphone example data

--- amazoncommentfetcher.py
+++ amazoncommentfetcher.py
@@ -240,8 +240,8 @@ def write2File(comments):
 
     with open(filename_out, 'w') as f:
         for comment in comments:
-            f.write("%s|%s" % (category, comment))
-            f.write("\n---\n")
+            f.write("%s|%s\n" % (category, comment))
+            #f.write("\n---\n")
 
 if __name__ == "__main__":
     if len(argv) == 3:
